42fb7619d76ebc32c6b9de13efc533c72470e673
Abel ADARY <abel.adary@nerd.nintendo.com>
2025-01-19 14:41:09 +0100

    feat: add caching of inference

--- aipuzzle/img_embedder.py
+++ aipuzzle/img_embedder.py
@@ -7,7 +7,7 @@ import torch
 from timm.data import resolve_data_config  # type: ignore
 from timm.data.transforms_factory import create_transform
 
-from aipuzzle.puzzle import Piece, Side
+from aipuzzle.puzzle import Piece, PieceID, Side
 
 
 class KeyQueryPredicter(Protocol):
@@ -17,7 +17,7 @@ class KeyQueryPredicter(Protocol):
 
 # TODO: add caching
 class PretrainedImgEmbedder(KeyQueryPredicter):
-    """Returning a copy of embedding on all sides"""
+    """Returning a copy of the same embedding on all sides, for query and keys"""
 
     def __init__(self, model_name: str, input_resolution: tuple[int, int], device: str = "cpu"):
         self._model: torch.nn.Module = timm.create_model(model_name, pretrained=True)  # type: ignore
@@ -28,14 +28,38 @@ class PretrainedImgEmbedder(KeyQueryPredicter):
         self._model.to(device)
         self._model.eval()
 
-    def get_query(self, piece: Piece, sides: set[Side]) -> dict[Side, npt.NDArray[np.float32]]:
-        model_input = self._transform(piece.texture).unsqueeze(0)  # type: ignore
+        self._inference_cache: dict[PieceID, npt.NDArray[np.float32]] = {}
+
+    def _update_inference_cache(self, pieces: list[Piece]) -> None:
+        is_in_cache = [piece.id_ in self._inference_cache for piece in pieces]
+        if all(is_in_cache):
+            return
+
+        model_input = torch.stack(
+            [self._transform(piece.texture) for (in_cache, piece) in zip(is_in_cache, pieces) if not in_cache],  # type: ignore
+            dim=0,
+        )
+
         with torch.no_grad():
-            out = self._model(model_input).cpu().numpy()[0]
-        return {side: out.copy() for side in sides}
+            new_embeddings = self._model(model_input).cpu().numpy()
+
+        embedding_id = 0
+        for piece, is_in_cache in zip(pieces, is_in_cache):
+            if is_in_cache:
+                continue
+            self._inference_cache[piece.id_] = new_embeddings[embedding_id]
+            embedding_id += 1
+
+        return
+
+    def get_query(self, piece: Piece, sides: set[Side]) -> dict[Side, npt.NDArray[np.float32]]:
+        self._update_inference_cache([piece])
+        return {side: self._inference_cache[piece.id_].copy() for side in sides}
 
     def get_keys(self, pieces: list[Piece], sides: set[Side]) -> dict[Side, npt.NDArray[np.float32]]:
-        model_input = torch.stack([self._transform(piece.texture) for piece in pieces], dim=0)  # type: ignore
-        with torch.no_grad():
-            out = self._model(model_input).cpu().numpy()
-        return {side: out.copy() for side in sides}
+        self._update_inference_cache(pieces)
+        embeddings = np.stack(
+            [self._inference_cache[piece.id_] for piece in pieces],
+            axis=0,
+        )
+        return {side: embeddings.copy() for side in sides}
